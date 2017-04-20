951eaebd80e08c60cf917fb11a97ecef2afe200c
Santi Pagola <santipagola@gmail.com>
2017-04-20 15:38:56 +0200

    added certificate generator & custom nginx.conf

---
+++ recipes-scripts/certgen/files/certgen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DIR="/etc/nginx"
+CERT="$DIR/mysland.pem"
+KEY="$DIR/mysland.key"
+# Generate CSR & private key
+
+if [ ! -f $KEY ] || [ ! -f $CERT ]
+then
+	openssl req -newkey rsa:2048 -nodes -keyout $KEY \
+		-x509 -days 365 -out $CERT
+else
+	while(true)
+	do
+		echo "key and/or domain files exist. replace? [y/n]:"
+		read ans
+		case $ans in
+		y|Y)
+			openssl req -newkey rsa:2048 -nodes -keyout $KEY \
+                -x509 -days 365 -out $CERT
+		break
+		;;
+		n|N)
+			echo "Aborting ..."
+		;;
+		*)
+			echo "Wrong option"
+		;;
+		esac
+	done
+fi
