c627830c3ed4c57daa851d56a9ace81e0513682b
Santi Pagola <santipagola@gmail.com>
2017-04-20 15:46:51 +0200

    last check just in case key/cert names dont match

--- recipes-scripts/certgen/files/certgen.sh
+++ recipes-scripts/certgen/files/certgen.sh
@@ -29,3 +29,14 @@ else
 		esac
 	done
 fi
+
+# Last check, to see if generated names match https server
+if [ -z $(grep $KEY nginx.conf) ] || [ -z $(grep $CERT nginx.conf) ]
+then
+	echo "Check that the generated key/cert matches the name in nginx.conf file"
+	exit 1
+else
+	# Match, then copy nginx.conf to /etc/nginx
+	cp ./nginx.conf /etc/nginx
+	echo 'Copied custom nginx.conf to /etc/nginx'
+fi
